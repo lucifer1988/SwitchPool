1f38ab40520743933f813d2f9812adfe2c6304ca
alientest <alienetest123@163.com>
2015-01-14 15:09:20 +0800

    login UI

--- SwitchPool/gen/com/xiaoshuye/switchpool/R.java
+++ SwitchPool/gen/com/xiaoshuye/switchpool/R.java
@@ -1082,12 +1082,15 @@ containing a value of this type.
         public static final int login_field_bg=0x7f020059;
         public static final int login_login_highlight=0x7f02005a;
         public static final int login_login_normal=0x7f02005b;
-        public static final int login_more_highlight=0x7f02005c;
-        public static final int login_more_normal=0x7f02005d;
-        public static final int login_password=0x7f02005e;
-        public static final int login_username=0x7f02005f;
+        public static final int login_login_selecter=0x7f02005c;
+        public static final int login_more_highlight=0x7f02005d;
+        public static final int login_more_normal=0x7f02005e;
+        public static final int login_more_selecter=0x7f02005f;
+        public static final int login_password=0x7f020060;
+        public static final int login_username=0x7f020061;
     }
     public static final class id {
+        public static final int Button01=0x7f050043;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1099,16 +1102,19 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050042;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int editText_login_password=0x7f050040;
+        public static final int editText_login_username=0x7f05003e;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1118,6 +1124,10 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView_login_password=0x7f05003f;
+        public static final int imageView_login_username=0x7f05003d;
+        public static final int linearLayout_login_button=0x7f050041;
+        public static final int linearLayout_login_field=0x7f05003c;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1207,6 +1217,14 @@ containing a value of this type.
         public static final int home=0x7f0c0000;
     }
     public static final class string {
+        public static final int Login_failed=0x7f0a0014;
+        public static final int Login_succeeded=0x7f0a0015;
+        /**  utility
+ utility
+         */
+        public static final int OK=0x7f0a0016;
+        public static final int Password_will_not_be_nil=0x7f0a0013;
+        public static final int Username_will_not_be_nil=0x7f0a0012;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1247,9 +1265,14 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int login_login_btn=0x7f0a0011;
+        public static final int login_password_hint=0x7f0a0010;
+        /**  login
+login
+         */
+        public static final int login_username_hint=0x7f0a000f;
     }
     public static final class style {
         /** 
