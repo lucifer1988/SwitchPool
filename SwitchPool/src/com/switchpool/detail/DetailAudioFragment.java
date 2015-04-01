7adb5a4f81c611350877bb631ebc81f319ecdff4
alientest <alienetest123@163.com>
2015-04-01 17:35:53 +0800

    audio player UI --liuyi

---
+++ SwitchPool/src/com/switchpool/detail/DetailAudioFragment.java
@@ -0,0 +1,19 @@
+package com.switchpool.detail;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class DetailAudioFragment extends Fragment implements OnClickListener {
+
+	public DetailAudioFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
